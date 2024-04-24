417536e3ee429b8110fb7e320aade34ca70f1dd4
atlas <a.sproson@outlook.com>
2024-04-24 19:43:22 +0100

    Adds base tag styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,38 @@ import "./App.css";
 const projects = [
   {
     id: 0,
-    name: "Project 1",
+    name: "Pixel Perfect",
+    description: "awdawd",
+    tags: ["React", "Tailwind"],
   },
   {
     id: 1,
-    name: "Project 2",
+    name: "AI Maze Solver",
+    description: "awdawd",
+    tags: ["Python", "AI", "DSA"],
   },
   {
     id: 2,
-    name: "Project 3",
+    name: "CLI Adventure Game",
+    description: "awdawd",
+    tags: ["Python", "Bash"],
   },
   {
     id: 3,
-    name: "Project 4",
+    name: "CRUD ToDos",
+    description: "awdawd",
+    tags: ["React", "Tailwind", "NodeJS", "MongoDB"],
   },
 ];
 
 function App() {
   return (
-    <div className="grid grid-cols-1 border-2 border-red-500 lg:px-40 xl:px-72 2xl:px-96">
-      <div className="grid grid-cols-2">
-        {projects.map(({ id, name }) => {
+    <div className="grid grid-cols-1 lg:px-40 xl:px-72 2xl:px-96">
+      <div className="grid sm:grid-cols-2">
+        {projects.map(({ id, name, description, tags }) => {
           return (
-            <div
-              key={id}
-              className="text-white border-2 border-green-500 text-center"
-            >
-              <Card name={name} />
+            <div key={id} className="text-white border-2 border-green-500">
+              <Card name={name} description={description} tags={tags} />
             </div>
           );
         })}
@@ -42,12 +47,32 @@ export default App;
 
 interface CardProps {
   name: string;
+  description: string;
+  tags: string[];
 }
 
-const Card = ({ name }: CardProps) => {
+const Card = ({ name, description, tags }: CardProps) => {
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className="text-white">
+      <h1 className="text-center">{name}</h1>
+      <div className="flex space-x-2 justify-center my-2">
+        {tags.map((tag, idx) => (
+          <Tag key={idx} tag={tag} />
+        ))}
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+interface TagProps {
+  tag: string;
+}
+
+const Tag = ({ tag }: TagProps) => {
+  return (
+    <div className="bg-cyan-500 rounded-xl px-3">
+      <p className="py-1">{tag}</p>
     </div>
   );
 };
